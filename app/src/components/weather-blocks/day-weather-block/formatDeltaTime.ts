7285f313947180b052351e919baf85d8b6b498d0
Trequend <68006090+Trequend@users.noreply.github.com>
2021-11-27 18:49:54 +0300

    feat(app): change delta date format

--- app/src/components/weather-blocks/day-weather-block/formatDeltaTime.ts
+++ app/src/components/weather-blocks/day-weather-block/formatDeltaTime.ts
@@ -1,6 +1,10 @@
 export function formatDeltaTime(date: Date, otherDate: Date) {
   const delta = otherDate.getTime() - date.getTime();
   const hours = Math.floor(delta / 1000 / 60 / 60);
-  const minutes = Math.ceil(delta / 1000 / 60) - hours * 60;
-  return `${hours}H ${minutes}M`;
+  const minutes = Math.floor(delta / 1000 / 60) - hours * 60;
+  if (hours === 0) {
+    return `${minutes}M`;
+  } else {
+    return `${hours}H ${minutes}M`;
+  }
 }
