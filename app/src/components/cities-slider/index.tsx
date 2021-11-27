a713c5e0d35820e5d98971a12faafbbb3ab41ecb
Trequend <68006090+Trequend@users.noreply.github.com>
2021-11-27 18:42:51 +0300

    feat(app): add empty list message

--- app/src/components/cities-slider/index.tsx
+++ app/src/components/cities-slider/index.tsx
@@ -10,7 +10,12 @@ SwiperCore.use([Pagination]);
 export const CitiesSlider: FC = () => {
   const { ids, removeId } = useCitiesIdsList();
 
-  return (
+  return ids.length === 0 ? (
+    <div className={classes.empty}>
+      <h2>No cities</h2>
+      <p>Use search to add cities</p>
+    </div>
+  ) : (
     <Swiper pagination spaceBetween={30} className={classes.swiper}>
       {ids.map((id) => (
         <SwiperSlide key={id}>
